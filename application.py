de3d082987b7d2f1332b8fc97b1371825f1dd036
Baibhav Mishra <71711792+baibhavmishra785@users.noreply.github.com>
2023-07-25 12:40:20 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template,jsonify
+rom flask import Flask, request, render_template,jsonify
 from flask_cors import CORS,cross_origin
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
