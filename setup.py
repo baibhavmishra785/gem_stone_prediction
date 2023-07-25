0a9f63b4cbfcdda8b05564d91b6392fc7c44bd3c
Baibhav Mishra <baibhavmishra6@gmail.com>
2023-07-25 11:58:15 +0530

    files added

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name = 'mlproject_regression',
     version= '0.0.1',
-    author='Utkarsh Gaikwad',
-    author_email='gaikwadujg@gmail.com',
+    author='Baibhav Mishra',
+    author_email='baibhavmishra6@gmail.com',
     packages = find_packages(),
     install_requires = get_requirements('requirements.txt')
 )
